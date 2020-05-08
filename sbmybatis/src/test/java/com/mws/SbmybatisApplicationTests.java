68d20f28ee7ae2edb6ec0f285d567c7431984d05
mawenshuai <1071179404@qq.com>
2020-05-08 17:06:50 +0800

    mybatis自定义typehandler

---
+++ sbmybatis/src/test/java/com/mws/SbmybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
